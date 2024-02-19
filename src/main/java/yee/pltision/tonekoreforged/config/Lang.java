c071f59207dde862810eb9d21cb87c4f5be61f06
pltision <110087936+pltision@users.noreply.github.com>
2024-02-20 06:59:01 +0800

    发布

--- src/main/java/yee/pltision/tonekoreforged/config/Lang.java
+++ src/main/java/yee/pltision/tonekoreforged/config/Lang.java
@@ -15,13 +15,13 @@ public class Lang {
 
         GET_NEKO_ALREADY=new ConfigLang("commands.toneko.get_neko.already", "This player is already your neko","这个玩家已经是你的猫娘了"),
         GET_NEKO_INFO=new ConfigLang("commands.toneko.get_neko.info", " is your neko now","现在是你的猫娘了"),
-        GET_NEKO_REQUEST=new ConfigLang("commands.toneko.get_neko.request_info", " want let you be their neko","想让你称为他的猫娘"),
+        GET_NEKO_REQUEST=new ConfigLang("commands.toneko.get_neko.request_info", " want let you be their neko","想让你成为他的猫娘"),
         REMOVE_REQUEST=new ConfigLang("commands.toneko.remove_request", " want remove your connect","想与你断开联系"),
 
         LIST_OWNER_INFO=new ConfigLang("commands.toneko.list_owner.info", "Your owners include: ","你的主人包括: "),
         GET_OWNER_ALREADY=new ConfigLang("commands.toneko.get_owner.already", "This player is already your owner","这个玩家已经是你的主人了"),
         GET_OWNER_INFO=new ConfigLang("commands.toneko.get_owner.info", " is your owner now","现在是你的主人了"),
-        GET_OWNER_REQUEST=new ConfigLang("commands.toneko.get_owner.request_info", " want let you be their owner","想让你称为他的主人"),
+        GET_OWNER_REQUEST=new ConfigLang("commands.toneko.get_owner.request_info", " want let you be their owner","想让你成为他的主人"),
 
         REMOVE_NEKO_NOT_FOUND=new ConfigLang("commands.toneko.remove_neko.not_found", "You have no neko whit this UUID or name","你没有这个UUID或名称的猫娘"),
         REMOVE_NEKO_INFO=new ConfigLang("commands.toneko.remove_neko.info", " is not your neko now","现在不是你的猫娘了"),
