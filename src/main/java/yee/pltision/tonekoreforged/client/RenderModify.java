af1336f1e5c284e30f03de1d804a66a556243261
pltision <110087936+pltision@users.noreply.github.com>
2024-06-15 01:37:52 +0800

    暂时禁用CollarLayout

--- src/main/java/yee/pltision/tonekoreforged/client/RenderModify.java
+++ src/main/java/yee/pltision/tonekoreforged/client/RenderModify.java
@@ -2,15 +2,13 @@ package yee.pltision.tonekoreforged.client;
 
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 import static yee.pltision.tonekoreforged.ToNeko.MODID;
 
-@Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+//@Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class RenderModify {
 
 
